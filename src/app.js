547f7d6606ad9207ea145a0ae7fdebfa93214f1f
Russell Schmidt <russell.schmidt@me.com>
2018-04-04 15:42:09 -0700

    add internal NavLinks and Links, 404, and better meta data

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {BrowserRouter, Route, Switch} from 'react-router-dom'
+import {BrowserRouter, Route, Switch, Link, NavLink} from 'react-router-dom'
 import 'normalize.css/normalize.css'
 import './styles/styles.scss'
 
@@ -30,19 +30,34 @@ const HelpPage = () => (
 
 const NotFoundPage = () => (
   <div>
-    404! Not Found
+    404! Not Found <Link to="/">Home</Link>
   </div>
+)
+
+const Header = () => (
+  <header>
+    <h1>Expensify</h1>
+    <nav>
+      <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink>
+      <NavLink to="/create" activeClassName="is-active">Create</NavLink>
+      <NavLink to="/edit" activeClassName="is-active">Edit</NavLink>
+      <NavLink to="/help" activeClassName="is-active">Help</NavLink>
+    </nav>
+  </header>
 )
 
 const routes = (
   <BrowserRouter>
-    <Switch>
-      <Route path="/" component={ExpenseDashboardPage} exact={true}/>
-      <Route path="/create" component={AddExpensePage} />
-      <Route path="/edit" component={EditExpensePage} />
-      <Route path="/help" component={HelpPage} />
-      <Route component={NotFoundPage} />
-    </Switch>
+    <div>
+      <Header />
+      <Switch>
+        <Route path="/" component={ExpenseDashboardPage} exact={true}/>
+        <Route path="/create" component={AddExpensePage} />
+        <Route path="/edit" component={EditExpensePage} />
+        <Route path="/help" component={HelpPage} />
+        <Route component={NotFoundPage} />
+      </Switch>
+    </div>
   </BrowserRouter>
 )
 
