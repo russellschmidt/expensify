371d88e59e8dd0986b7623d91abe25b84c73f619
Russell Schmidt <russell.schmidt@me.com>
2018-04-04 12:25:51 -0700

    first commit

--- webpack.config.js
+++ webpack.config.js
@@ -25,33 +25,9 @@ module.exports = (env) => {
     },
     devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
     devServer: {
-      contentBase: path.join(__dirname, 'public')
+      contentBase: path.join(__dirname, 'public'),
+      historyApiFallback: true
     }
   }
 }
 
-// module.exports = {
-//   entry: './src/app.js',
-//   output: {
-//     path: path.join(__dirname, 'public'),
-//     filename: 'bundle.js'
-//   },
-//   module: {
-//     rules: [{
-//       loader: 'babel-loader',
-//       test: /\.js$/,
-//       exclude: /node_modules/
-//     }, {
-//       test: /\.s?css$/,
-//       use: [
-//         'style-loader',
-//         'css-loader',
-//         'sass-loader'
-//       ]
-//     }]
-//   },
-//   devtool: 'cheap-module-eval-source-map',
-//   devServer: {
-//     contentBase: path.join(__dirname, 'public')
-//   }
-// }
