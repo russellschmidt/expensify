627018e822408980986b7b7f9c97f1cd2df2f757
Russell Schmidt <russell.schmidt@me.com>
2018-04-05 11:35:36 -0700

    add header, app router adjustments

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,9 +1,15 @@
 import React from 'react'
 
-const EditExpensePage = () => (
-  <div>
-    This is from my EditExpensePage component
-  </div>
-)
+const EditExpensePage = (props) => {
+  console.log(props)
+  return (
+    <div>
+      This is my Edit Expense Page
+
+      Editing the expense with id of {props.match.params.id}
+    </div>
+  )
+}
+
 
 export default EditExpensePage
