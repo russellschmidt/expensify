fadbc4dd5236ada5226f856d32353189cdb29b97
Russell Schmidt <russell.schmidt@me.com>
2018-04-07 19:51:50 -0700

    add removeExpense action and reducer case

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -14,6 +14,11 @@ const addExpense = ({ description = '', note = '', amount = 0, createdAt = 0} = 
 })
 
 // Remove Expense
+const removeExpense = ({id} = {}) => ({
+  type: 'REMOVE_EXPENSE',
+  id
+})
+
 // Edit Expense
 // Set text filter
 // sort by date
@@ -26,7 +31,12 @@ const expensesReducerDefaultState = []
 const expensesReducer = (state = expensesReducerDefaultState, action) => {
   switch (action.type) {
     case 'ADD_EXPENSE':
-      return state.concat(action.expense)
+      return [
+        ...state,
+        action.expense
+      ]
+    case 'REMOVE_EXPENSE':
+      return state.filter(({ id }) => id !== action.id)
     default:
       return state
   }
@@ -57,7 +67,11 @@ store.subscribe(() => {
   console.log(store.getState())
 })
 
-store.dispatch(addExpense({ description: 'Rent', amount: 100}))
+const expenseOne = store.dispatch(addExpense({ description: 'Rent', amount: 100}))
+const expenseTwo = store.dispatch(addExpense({ description: 'Coffee', amount: 200}))
+const expenseThree = store.dispatch(addExpense({ description: 'Cum', amount: 300}))
+store.dispatch(removeExpense({ id: expenseOne.expense.id }))
+store.dispatch(removeExpense({ id: expenseThree.expense.id }))
 
 const demoState = {
   expenses: [{
