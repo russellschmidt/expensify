4b4bdaa2c28f6a97a30a3bd39c30c6eb3b43ec81
Russell Schmidt <russell.schmidt@me.com>
2018-04-07 18:38:39 -0700

    add test reducers, use combineReducers

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -1,5 +1,46 @@
 import { createStore, combineReducers } from 'redux'
 
+// Add Expense
+// Remove Expense
+// Edit Expense
+// Set text filter
+// sort by date
+// sort by amount
+// set start date
+// set end date
+
+// Expenses reducer
+const expensesReducerDefaultState = []
+const expensesReducer = (state = expensesReducerDefaultState, action) => {
+  switch (action.type) {
+    default:
+      return state
+  }
+}
+
+const filtersReducerDefaultState = {
+  text: '',
+  sortBy: 'date',
+  startDate: undefined,
+  endDate: undefined
+}
+
+const filtersReducer = (state = filtersReducerDefaultState, action) => {
+  switch (action.type) {
+    default: 
+      return state
+  }
+}
+
+const store = createStore(
+  combineReducers({
+    expenses: expensesReducer,
+    filters: filtersReducer
+  })
+)
+
+console.log(store.getState())
+
 const demoState = {
   expenses: [{
     id: 'abc123',
