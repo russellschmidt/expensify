3c237b5eb93caa0fc23927ea594cec6e37e712ce
Russell Schmidt <russell.schmidt@me.com>
2018-04-13 18:25:40 -0700

    complete testing of reducers/expenses

---
+++ src/tests/reducers/expenses.test.js
@@ -0,0 +1,68 @@
+import expensesReducer from '../../reducers/expenses'
+import expenses from '../fixtures/expenses'
+
+test('should set default state', () => {
+  const state = expensesReducer(undefined, {type: '@@INIT'})
+  expect(state).toEqual([])
+})
+
+test('should add expense', () => {
+  const action = {
+    type: 'ADD_EXPENSE',
+    expense: {
+      id: expenses[expenses.length - 1].id + 1,
+      amount: 123456,
+      description: 'fubu gear',
+      note: 'the best',
+      createdAt: 300000000
+    }
+  }
+  const state = expensesReducer(expenses, action)
+  expect(state).toEqual([...expenses, action.expense])
+})
+
+test('should edit_expense with valid id', () => {
+  const action = {
+    type: 'EDIT_EXPENSE',
+    id: expenses[2].id,
+    updates: {
+      amount: 987654,
+      description: 'magician supplies',
+      note: 'rabbits, hats, chainsaw',
+      createdAt: 350000000
+    }
+  }
+  const state = expensesReducer(expenses, action)
+  expect(state).toEqual([ expenses[0], expenses[1], {id: action.id, ...action.updates} ])
+})
+
+test('should not edit_expense if id not found', () => {
+  const action = {
+    type: 'EDIT_EXPENSE',
+    id: -1,
+    updates: {
+      amount: 12345,
+      description: 'parallelogram'
+    }
+  }
+  const state = expensesReducer(expenses, action)
+  expect(state).toEqual(state)
+})
+
+test('should remove expense if id found', () => {
+  const action = {
+    type: 'REMOVE_EXPENSE',
+    id: expenses[1].id
+  }
+  const state = expensesReducer(expenses, action)
+  expect(state).toEqual([ expenses[0], expenses[2] ])
+})
+
+test('should NOT remove expense if id not found', () => {
+  const action = {
+    type: 'REMOVE_EXPENSE',
+    id: -1
+  }
+  const state = expensesReducer(expenses, action)
+  expect(state).toEqual([ expenses[0], expenses[1], expenses[2] ])
+})
--- src/tests/selectors/expenses.test.js
+++ src/tests/selectors/expenses.test.js
@@ -1,11 +1,6 @@
-import selectExpenses from '../../selectors/expenses'
 import moment from 'moment'
-
-const expenses = [
-  { id: 1, description: 'gum', note: '', amount: 195, createdAt: 0},
-  { id: 2, description: 'rent', note: 'hey', amount: 109500, createdAt: moment(0).subtract(4, 'days').valueOf()},
-  { id: 3, description: 'credit card', note: 'nope', amount: 4500, createdAt: moment(0).add(4, 'days').valueOf()}
-]
+import selectExpenses from '../../selectors/expenses'
+import expenses from '../fixtures/expenses'
 
 test('should filter by text value', () => {
   const filters = {
