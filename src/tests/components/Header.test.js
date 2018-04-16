aea4f2a7d2c88884c7e9f87474c323d5daa4ad35
Russell Schmidt <russell.schmidt@me.com>
2018-04-16 10:07:25 -0700

    downgrade enzyme-adapter-react-15, add enzyme-to-json@3.0.1

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,11 +1,14 @@
 import React from 'react'
-import ReactShallowRenderer from 'react-test-renderer/shallow'
+import { shallow } from 'enzyme'
 
 import Header from '../../components/Header'
 
 test('should render header correctly', () => {
-  const renderer = new ReactShallowRenderer()
-  renderer.render(<Header />)
-  expect(renderer.getRenderOutput()).toMatchSnapshot()
-  // console.log(renderer.getRenderOutput())
+  const wrapper = shallow(<Header/>)
+  expect(wrapper).toMatchSnapshot()
+
+  // const renderer = new ReactShallowRenderer()
+  // renderer.render(<Header />)
+  // expect(renderer.getRenderOutput()).toMatchSnapshot()
+  // // console.log(renderer.getRenderOutput())
 })
