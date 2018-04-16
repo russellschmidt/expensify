79920b158749205f5528f91341eeea7342eeb238
Russell Schmidt <russell.schmidt@me.com>
2018-04-16 12:06:23 -0700

    add test for NotFoundPage

--- src/tests/components/ExpenseListItem.test.js
+++ src/tests/components/ExpenseListItem.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { shallow } from 'enzyme'
-import { ExpenseListItem } from '../../components/ExpenseListItem'
+import ExpenseListItem from '../../components/ExpenseListItem'
 import expenses from '../fixtures/expenses'
 
 test('should render ExpenseListItem with expense', () => {
