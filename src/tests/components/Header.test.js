b3af05799be390ac948f4d093c8ea4acac980491
Russell Schmidt <russell.schmidt@me.com>
2018-04-16 11:01:34 -0700

    got tests running

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,14 +1,8 @@
 import React from 'react'
 import { shallow } from 'enzyme'
-
 import Header from '../../components/Header'
 
 test('should render header correctly', () => {
   const wrapper = shallow(<Header/>)
   expect(wrapper).toMatchSnapshot()
-
-  // const renderer = new ReactShallowRenderer()
-  // renderer.render(<Header />)
-  // expect(renderer.getRenderOutput()).toMatchSnapshot()
-  // // console.log(renderer.getRenderOutput())
 })
