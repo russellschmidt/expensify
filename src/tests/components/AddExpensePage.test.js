37e2c210db9b6cc49de0ac59b1a09feaaf5afd16
Russell Schmidt <russell.schmidt@me.com>
2018-04-17 14:29:04 -0700

    refactored addExpensePage again and add in test page for editExpensePage

--- src/tests/components/AddExpensePage.test.js
+++ src/tests/components/AddExpensePage.test.js
@@ -8,7 +8,7 @@ let addExpense, history, wrapper
 beforeEach(() => {
   addExpense = jest.fn()
   history = { push: jest.fn() }
-  wrapper = shallow(<AddExpensePage addExpense={onSubmit} history={history} />)
+  wrapper = shallow(<AddExpensePage addExpense={addExpense} history={history} />)
 })
 
 test('should render AddExpensePage correctly', () => {
--- src/tests/components/EditExpensePage.test.js
+++ src/tests/components/EditExpensePage.test.js
@@ -3,20 +3,34 @@ import { shallow } from 'enzyme'
 import { EditExpensePage } from '../../components/EditExpensePage'
 import expenses from '../fixtures/expenses'
 
-let editExpense, history, wrapper
+let editExpense, removeExpense, history, wrapper
 
 beforeEach(() => {
   editExpense = jest.fn()
+  removeExpense = jest.fn()
   history = {push: jest.fn()}
-  wrapper = shallow(<EditExpensePage editExpense={onSubmit} history={history}/>)
+  wrapper = shallow(
+    <EditExpensePage 
+      editExpense={editExpense} 
+      removeExpense={removeExpense} 
+      history={history}
+      expense={expenses[1]}
+    />
+  )
 })
 
-test('should render AddExpensePage correctly', () => {
+test('should render EditExpensePage correctly', () => {
   expect(wrapper).toMatchSnapshot()
 })
 
-test('should handle onSubmit', () => {
+test('should handle editExpense', () => {
   wrapper.find('ExpenseForm').prop('onSubmit')(expenses[1])
   expect(history.push).toHaveBeenLastCalledWith('/')
-  expect(addExpense).toHaveBeenLastCalledWith(expenses[1]) 
+  expect(editExpense).toHaveBeenLastCalledWith(expenses[1].id, expenses[1]) 
+})
+
+test('should handle removeExpense', () => {
+  wrapper.find('button').simulate('click')
+  expect(history.push).toHaveBeenLastCalledWith('/')
+  expect(removeExpense).toHaveBeenLastCalledWith(expenses[1])
 })
