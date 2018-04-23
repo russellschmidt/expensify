81120d7ec0818c33717d2a2e860fad1cb9f462ed
Russell Schmidt <russell.schmidt@me.com>
2018-04-23 14:16:14 -0700

    change number formatting, add numeral library

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import moment from 'moment'
+import numeral from 'numeral'
 
-const ExpenseListItem = (props) => (
+const ExpenseListItem = ({id, description, amount, createdAt}) => (
   <tr>
-    <td><Link to={`/edit/${props.id}`}>{props.description}</Link></td>
-    <td>${(props.amount / 100).toFixed(2)}</td>
-    <td>{props.createdAt}</td>
+    <td><Link to={`/edit/${id}`}>{description}</Link></td>
+    <td>{numeral(amount / 100).format('$0, 0.00')}</td>
+    <td>{moment(createdAt).format('MMM Do YYYY')}</td>
   </tr>
 )
 
