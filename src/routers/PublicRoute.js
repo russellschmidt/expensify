2c289d793e3806a810787125fca3145d5085b08a
Russell Schmidt <russell.schmidt@me.com>
2018-04-30 12:03:31 -0700

    delete HelpPage, create PublicRoute

--- src/routers/PublicRoute.js
+++ src/routers/PublicRoute.js
@@ -3,7 +3,7 @@ import {connect} from 'react-redux'
 import {Route, Redirect} from 'react-router-dom'
 import Header from '../components/Header'
 
-export const PrivateRoute = ({isAuthenticated, component: Component, ...rest}) => (
+export const PublicRoute = ({isAuthenticated, component: Component, ...rest}) => (
   <Route {...rest} component={(props) => (
     isAuthenticated ? (
       <div>
@@ -20,4 +20,4 @@ const mapStateToProps = (state) => ({
   isAuthenticated: !!state.auth.uid
 })
 
-export default connect(mapStateToProps)(PrivateRoute)
+export default connect(mapStateToProps)(PublicRoute)
