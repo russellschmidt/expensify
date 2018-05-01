7e1c5fa0ad708d7bc9ca6eced314b56e4a0e9318
Russell Schmidt <russell.schmidt@me.com>
2018-05-01 11:26:33 -0700

    add LoadingPage, update snapshots

--- src/app.js
+++ src/app.js
@@ -9,8 +9,8 @@ import configureStore from './store/configureStore'
 import { startSetExpenses, removeExpense, editExpense } from './actions/expenses'
 import { login, logout } from './actions/auth'
 import getVisibleExpenses from './selectors/expenses'
-
 import {firebase} from './firebase/firebase'
+import LoadingPage from './components/LoadingPage'
 
 const store = configureStore()
 
@@ -33,7 +33,7 @@ const renderApp = () => {
   }
 }
 
-ReactDOM.render(<p>Loading...</p>, document.getElementById('app'))
+ReactDOM.render(<LoadingPage/>, document.getElementById('app'))
 
 firebase.auth().onAuthStateChanged((user) => {
   if (user) {
