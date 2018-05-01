eca7c656faf9598ce01d329c19b4c49a07dd61f7
Russell Schmidt <russell.schmidt@me.com>
2018-04-30 17:49:06 -0700

    style home page

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -4,8 +4,12 @@ import {startLogin} from '../actions/auth'
 
 
 export const LoginPage = ({startLogin}) => (
-  <div>
-    <button onClick={startLogin}>Log in</button>
+  <div className="box-layout">
+    <div className="box-layout__box">
+      <h1 className="box-layout__title">Expensify</h1>
+      <p>Track and control your expenses</p>
+      <button onClick={startLogin} className="box-layout__button">Login with Google</button>
+    </div>
   </div>
 )
 
